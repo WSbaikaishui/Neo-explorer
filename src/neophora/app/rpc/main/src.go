2275943d4d7d02ef45043f8a533b16ad7ff42550
vang1ong7ang <vang1ong7ang@outlook.com>
2020-12-07 10:53:20 +0000

    dev

--- src/neophora/app/rpc/main/src.go
+++ src/neophora/app/rpc/main/src.go
@@ -1,30 +1,42 @@
 package main
 
 import (
+	"io"
 	"log"
 	"neophora/biz"
-	"net"
+	"neophora/cli"
+	"neophora/lib/joh"
+	"neophora/lib/scex"
+	"net/http"
 	"net/rpc"
-	"net/rpc/jsonrpc"
 	"os"
+	"strings"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", os.ExpandEnv("0.0.0.0:${RPC_PORT}"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println("[MAIN][Accept]", err)
-			continue
-		}
-		go jsonrpc.ServeConn(conn)
-	}
+	address := os.ExpandEnv("0.0.0.0:${RPC_PORT}")
+	http.ListenAndServe(address, &joh.T{
+		Method: http.MethodPost,
+		Codec: func(rwc io.ReadWriteCloser) rpc.ServerCodec {
+			ret := &scex.T{}
+			ret.Init(rwc)
+			ret.SetF(func(v string) string {
+				return v
+			})
+			return ret
+		},
+	})
 }
 
 func init() {
+	address := os.ExpandEnv("${DBS_ADDRESS}")
+	addresses := strings.Split(address, " ")
+	log.Println("[MAIN][INIT][DBS ADDRESS]", addresses)
 	rpc.Register(&biz.Version{})
-	rpc.Register(&biz.Data{})
+	rpc.Register(&biz.Data{
+		Client: &cli.T{
+			Addresses: addresses,
+			TryTimes:  3,
+		},
+	})
 }
