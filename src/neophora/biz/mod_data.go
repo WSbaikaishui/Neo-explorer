fb58f5f7b1f7acbdf946a51c846f79c171326e40
vang1ong7ang <vang1ong7ang@outlook.com>
2020-12-11 03:30:48 +0000

    dev

--- src/neophora/biz/mod_data.go
+++ src/neophora/biz/mod_data.go
@@ -1,7 +1,6 @@
 package biz
 
 import (
-	"encoding/hex"
 	"fmt"
 	"neophora/cli"
 	"neophora/var/stderr"
@@ -11,79 +10,6 @@ import (
 // Data ...
 type Data struct {
 	Client *cli.T
-}
-
-// GetHeaderByHeightInHex ...
-func (me *Data) GetHeaderByHeightInHex(args struct {
-	Height uint64
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "header",
-		Host:   "height",
-		Path:   fmt.Sprintf("/%016x", args.Height),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.Get", []byte(urs), &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
-	return nil
-}
-
-// GetHeaderByHashInHex ...
-func (me *Data) GetHeaderByHashInHex(args struct {
-	Hash string
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "header",
-		Host:   "hash",
-		Path:   fmt.Sprintf("/%s", args.Hash),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.Get", []byte(urs), &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
-	return nil
-}
-
-// GetStorageByDBKeyHeightInHex ...
-func (me *Data) GetStorageByDBKeyHeightInHex(args struct {
-	DBKey  string
-	Height uint64
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "storage",
-		Host:   "dbkey-height",
-		Path:   fmt.Sprintf("/%s/%016x", args.DBKey, args.Height),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.GetLast", struct {
-		Key    []byte
-		Prefix int
-	}{
-		Key:    []byte(urs),
-		Prefix: len(urs) - 16,
-	}, &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
-	return nil
 }
 
 // GetCountInUInt64 ...
@@ -122,78 +48,5 @@ func (me *Data) GetCountInUInt64(args struct {
 	fmt.Sscanf(key.Path, "/%x", &count)
 
 	*ret = count
-	return nil
-}
-
-// GetContractByHashHeightInHex ...
-func (me *Data) GetContractByHashHeightInHex(args struct {
-	Hash   string
-	Height uint64
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "contract",
-		Host:   "hash-height",
-		Path:   fmt.Sprintf("/%s/%016x", args.Hash, args.Height),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.GetLast", struct {
-		Key    []byte
-		Prefix int
-	}{
-		Key:    []byte(urs),
-		Prefix: len(urs) - 16,
-	}, &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
-	return nil
-}
-
-// GetHashByHeightInHex ...
-func (me *Data) GetHashByHeightInHex(args struct {
-	Height uint64
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "hash",
-		Host:   "height",
-		Path:   fmt.Sprintf("/%016x", args.Height),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.Get", []byte(urs), &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
-	return nil
-}
-
-// GetTransactionByHashInHex ...
-func (me *Data) GetTransactionByHashInHex(args struct {
-	Hash uint64
-}, ret *string) error {
-	uri := &url.URL{
-		Scheme: "tx",
-		Host:   "hash",
-		Path:   fmt.Sprintf("/%016x", args.Hash),
-	}
-
-	var result []byte
-
-	urs := uri.String()
-	if err := me.Client.Calls("DB.Get", []byte(urs), &result); err != nil {
-		return stderr.ErrUnknown
-	}
-
-	*ret = hex.EncodeToString(result)
-
 	return nil
 }
