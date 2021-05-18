3e6df6f4d52c7e062ef39e43188b0422b9b38945
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-05-18 16:11:22 +0800

    fixed

--- src/neophora/lib/cli/src.go
+++ src/neophora/lib/cli/src.go
@@ -132,33 +132,4 @@ func (me *T) Call(method string, args interface{}, reply interface{}) error {
 	fmt.Println(res)
 	return nil
 }
-
-//	var client *rpc.Client
-//	var err error
-//	select {
-//	case client = <-me.Pool:
-//	default:
-//		client, err = rpc.Dial("tcp", me.Address)
-//	}
-//	if err != nil {
-//		return err
-//	}
-//	if err = client.Call(method, args, reply); err != nil {
-//		client.Close()
-//		return err
-//	}
-//	select {
-//	case me.Pool <- client:
-//	default:
-//		client.Close()
-//	}
-//	return nil
-//}
-//
-//// Close ...
-//func (me *T) Close() {
-//	close(me.Pool)
-//	for client := range me.Pool {
-//		client.Close()
-//	}
 //}
