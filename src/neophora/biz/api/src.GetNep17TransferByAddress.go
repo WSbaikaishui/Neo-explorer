8edb279a04fb7b646b509b7b920208acdaae0222
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-05-31 16:44:25 +0800

    fixed

---
+++ src/neophora/biz/api/src.GetNep17TransferByAddress.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"neophora/lib/type/h160"
+	"neophora/var/stderr"
+)
+
+func (me *T) GetNep17TransferByAddress(args struct {
+	Address h160.T
+	Limit        int64
+	Skip         int64
+}, ret *json.RawMessage) error {
+	if args.Address.Valid() == false {
+		return stderr.ErrInvalidArgs
+	}
+	_, err := me.Data.Client.QueryAll(struct {
+		Collection string
+		Index      string
+		Sort       bson.M
+		Filter     bson.M
+		Query      []string
+		Limit      int64
+		Skip       int64
+	}{
+		Collection: "TransferNotification",
+		Index:      "someIndex",
+		Sort:       bson.M{},
+		Filter: bson.M{"$or": []interface{}{
+			bson.M{"from": args.Address.Val()},
+			bson.M{"to": args.Address.Val()},
+		}},
+		Query:      []string{},
+		Limit:      args.Limit,
+		Skip:       args.Skip,
+	}, ret)
+	if err != nil {
+		return err
+	}
+	return nil
+}
