adfd9119769a29cb761477016e3b778a7715c84e
WSbaikaishui <46080358+WSbaikaishui@users.noreply.github.com>
2021-07-14 18:16:55 +0800

    GetBlockInfoList

---
+++ biz/api/src.GetBlockInfoList.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func (me *T) GetBlockInfoList(args struct {
+	Filter map[string]interface{}
+	Limit        int64
+	Skip         int64
+}, ret *json.RawMessage) error {
+
+	r1, _, err := me.Data.Client.QueryAll(
+		struct {
+			Collection string
+			Index      string
+			Sort       bson.M
+			Filter     bson.M
+			Query      []string
+			Limit      int64
+			Skip       int64
+		}{
+			Collection: "Block",
+			Index:      "someIndex",
+			Sort:       bson.M{"index":-1},
+			Filter: bson.M{},
+			Query: []string{"_id","index","size","timestamp","hash"},
+			Limit: args.Limit,
+			Skip: args.Skip,
+		}, ret)
+	if err != nil {
+		return err
+	}
+	r2 := make([]map[string]interface{}, 0)
+	for _, item := range r1 {
+		_, count, err := me.Data.Client.QueryAll(
+			struct {
+			Collection string
+			Index      string
+			Sort       bson.M
+			Filter     bson.M
+			Query      []string
+			Limit      int64
+			Skip       int64
+		}{  Collection: "[Block~Transaction(Transactions)]",
+			Index: "someIndex",
+			Sort: bson.M{},
+			Filter: bson.M{"ParentID":item["_id"],
+				}}, ret)
+		if err != nil {
+			return err
+		}
+		item["transactionNumber"] = count
+		//delete(item,"_id")
+		r2 = append(r2, item)
+
+	}
+	r, err := json.Marshal(r2)
+	if err != nil {
+		return err
+	}
+	*ret = json.RawMessage(r)
+	return nil
+}
