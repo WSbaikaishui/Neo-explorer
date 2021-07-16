77bcd6c32653789399517974ed26af595be6db6a
WSbaikaishui <46080358+WSbaikaishui@users.noreply.github.com>
2021-07-16 11:16:25 +0800

    qiehuanfenzhi

--- biz/api/src.GetBlockInfoByBlockHash.go
+++ biz/api/src.GetBlockInfoByBlockHash.go
@@ -31,33 +31,49 @@ func (me *T) GetBlockInfoByBlockHash(args struct {
 	if err != nil {
 		return err
 	}
-	print(json.Marshal(r1))
-				r3, err := me.Data.Client.QueryAggregate(struct {
-					Collection string
-					Index      string
-					Sort       bson.M
-					Filter     bson.M
-					Pipeline   []bson.M
-					Query      []string
-				}{
-					Collection: "Transaction",
-					Index:      "someIndex",
-					Pipeline: []bson.M{bson.M{"$lookup": bson.M{"from": "Asset", "localField": "ParentID", "foreignField": "_id", "as": "asset"}}},
-					Sort:       bson.M{},
-					Filter:     bson.M{},
-					Query:      []string{},
-				}, ret)
-				if err != nil {
-					return err
-				}
-				if len(r3) != 0{
-					r1["totalNetworkFee"] = r3[0]["systemFee"]
-					r1["totalSystemFee"] = r3[0]["networkFee"]
-				}else {
-					r1["totalNetworkFee"] = 0
-					r1["totalSystemFee"] = 0
-				}
-					r, err := json.Marshal(r1)
+	r2, err := me.Data.Client.QueryDocument(
+		struct {
+			Collection string
+			Index      string
+			Sort       bson.M
+			Filter     bson.M
+
+		}{  Collection: "[Block~Transaction(Transactions)]",
+			Index: "someIndex",
+			Sort: bson.M{},
+			Filter: bson.M{"ParentID":r1["_id"],
+			}}, ret)
+	if err != nil {
+		return err
+	}
+	r1["transactionNumber"] = r2["total counts"]
+	r3, err := me.Data.Client.QueryAggregate(struct {
+		Collection string
+		Index      string
+		Sort       bson.M
+		Filter     bson.M
+		Pipeline   []bson.M
+		Query      []string
+	}{
+		Collection: "Transaction",
+		Index:      "someIndex",
+		Pipeline: []bson.M{bson.M{"$match":bson.M{"blockhash":args.BlockHash}},
+			bson.M{"$group":bson.M{"_id":"$blockhash","systemFee":bson.M{"$sum":"$sysfee"},"networkFee":bson.M{"$sum":"$netfee"}}}},
+		Sort:       bson.M{},
+		Filter:     bson.M{},
+		Query:      []string{},
+	}, ret)
+	if err != nil {
+		return err
+	}
+	if len(r3) != 0{
+		r1["totalNetworkFee"] = r3[0]["systemFee"]
+		r1["totalSystemFee"] = r3[0]["networkFee"]
+	}else {
+		r1["totalNetworkFee"] = 0
+		r1["totalSystemFee"] = 0
+	}
+		r, err := json.Marshal(r1)
 	if err != nil {
 		return err
 	}
